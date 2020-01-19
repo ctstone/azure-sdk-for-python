704b360c109dbb28bc56f968339ef2d00b59abcf
Christopher Stone <chstone@microsoft.com>
2020-01-19 09:17:04 -0500

    add get_model

--- sdk/cognitiveservices/azure-cognitiveservices-formrecognizer/azure/ai/formrecognizer/aio/form_recognizer_client_async.py
+++ sdk/cognitiveservices/azure-cognitiveservices-formrecognizer/azure/ai/formrecognizer/aio/form_recognizer_client_async.py
@@ -3,8 +3,9 @@ from azure.core.pipeline import AsyncPipeline
 from azure.core.pipeline.policies import HeadersPolicy, ContentDecodePolicy
 from azure.core.pipeline.transport import HttpRequest, AioHttpTransport, AioHttpTransportResponse
 from .._policies import ApiKeyCredentialPolicy, FormEndpointPolicy
-from .._requests import create_list_models_request
-from .._serialization.responses import read_model_listing
+from .._requests import create_list_models_request, create_get_model_request
+from .._serialization.converters import read_model_listing, read_model
+from ..models import Model
 
 
 class FormRecognizerClientAsync:
@@ -19,8 +20,15 @@ class FormRecognizerClientAsync:
         self._pipeline = AsyncPipeline(transport, policies=policies)
 
     async def list_models(self, next_link: str = None):
-        request = create_list_models_request()
+        request = create_list_models_request(next_link=next_link)
         async with self._pipeline as pipeline:
             response = await pipeline.run(request)
             http_response = response.http_response  # type: AioHttpTransportResponse
             return read_model_listing(http_response)  # TODO: paging
+
+    async def get_model(self, model_id: str) -> Model:
+        request = create_get_model_request(model_id)
+        async with self._pipeline as pipeline:
+            response = await pipeline.run(request)
+            http_response = response.http_response  # type: AioHttpTransportResponse
+            return read_model(http_response)  # TODO: paging
