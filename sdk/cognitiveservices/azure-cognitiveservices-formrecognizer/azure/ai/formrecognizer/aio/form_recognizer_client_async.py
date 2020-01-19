5ec262b445488c4d8fde9405b96ad2745f55cd97
Christopher Stone <chstone@microsoft.com>
2020-01-19 09:36:47 -0500

    add async pagable

--- sdk/cognitiveservices/azure-cognitiveservices-formrecognizer/azure/ai/formrecognizer/aio/form_recognizer_client_async.py
+++ sdk/cognitiveservices/azure-cognitiveservices-formrecognizer/azure/ai/formrecognizer/aio/form_recognizer_client_async.py
@@ -1,11 +1,12 @@
-from typing import Dict
+from typing import Dict, Awaitable, Tuple, AsyncIterator
+from azure.core.async_paging import AsyncPageIterator
 from azure.core.pipeline import AsyncPipeline
 from azure.core.pipeline.policies import HeadersPolicy, ContentDecodePolicy
 from azure.core.pipeline.transport import HttpRequest, AioHttpTransport, AioHttpTransportResponse
 from .._policies import ApiKeyCredentialPolicy, FormEndpointPolicy
 from .._requests import create_list_models_request, create_get_model_request
 from .._serialization.converters import read_model_listing, read_model
-from ..models import Model
+from ..models import Model, ModelInfo
 
 
 class FormRecognizerClientAsync:
@@ -20,11 +21,19 @@ class FormRecognizerClientAsync:
         self._pipeline = AsyncPipeline(transport, policies=policies)
 
     async def list_models(self, next_link: str = None):
-        request = create_list_models_request(next_link=next_link)
-        async with self._pipeline as pipeline:
-            response = await pipeline.run(request)
-            http_response = response.http_response  # type: AioHttpTransportResponse
-            return read_model_listing(http_response)  # TODO: paging
+        async def get_next(continuation_token: str) -> Awaitable[AioHttpTransportResponse]:
+            request = create_list_models_request(next_link=next_link)
+            async with self._pipeline as pipeline:
+                response = await pipeline.run(request)
+                return response.http_response
+
+        async def extract_data(response: AioHttpTransportResponse) -> Awaitable[Tuple[str, AsyncIterator[ModelInfo]]]:
+            return read_model_listing(response)
+
+        return AsyncPageIterator[ModelInfo](
+            get_next=get_next,
+            extract_data=extract_data,
+            continuation_token=next_link)
 
     async def get_model(self, model_id: str) -> Model:
         request = create_get_model_request(model_id)
